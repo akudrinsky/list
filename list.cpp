7555bef949357eaf4bd69a50c3aef5c7f4c72570
Alexey Kudrinsky <kudrinsky.am@gmail.com>
2019-11-01 13:33:06 +0300

    big feature

--- list.cpp
+++ list.cpp
@@ -9,6 +9,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define Lt_construct(lst) List_Construct(&lst, #lst);
 
@@ -25,6 +26,7 @@ enum lt_errors_c {
     canary_t,
     canary_b,
     bad_hash,
+    calling_nowhere,
     destructed
 };
 
@@ -37,6 +39,7 @@ enum lt_errors_c {
 //! @param [in] prev - array, where you can understand which physical number is before a given one
 //! @param [in] head - physical number of first (logically) element
 //! @param [in] tail - physical number of last (logically) element
+//! @param [in] free - first free element in list
 //! @param [in] size - number of elements in stack
 //! @param [in] is_sorted - if list physical numbers correspond to logical ones
 //! @param [in] is_valid - if it is OK
@@ -60,11 +63,16 @@ struct List_t {
 
 void List_Construct (List_t* lst, char* lst_name = "lst");
 void Lt_destruct (List_t* lst);
+
 void Dump_f (List_t* lst, const char* file, int line,
              const char* func_name, char* info = "assert",
              bool is_silent = false, bool no_corr = false,
              const char* file_name = "list_dump.txt");
+void list_graph (List_t* lst, const char* pict_name = "list.png", const char* pict_type = "png");
 bool List_okey (List_t* lst);
+
+void insert_after (List_t* lst, int pos, Elem_t elem);
+void delete_after (List_t* lst, int pos);
 
 
 
@@ -80,13 +88,14 @@ void List_Construct (List_t* lst, char* lst_name) {
     lst->name = lst_name;
     lst->data = (Elem_t *)calloc (starting_size, sizeof(Elem_t));
     lst->next = (Elem_t *)calloc (starting_size, sizeof(Elem_t));
+    lst->prev = (Elem_t *)calloc (starting_size, sizeof(Elem_t));
 
-    for (int i = 1; i < starting_size - 1; ++i) {
+    for (int i = 1; i < starting_size; ++i) {
         lst->next[i] = i + 1;
+        lst->prev[i] = -1;
     }
     lst->next[starting_size - 1] = 0;
 
-    lst->prev = (Elem_t *)calloc (starting_size, sizeof(Elem_t));
     lst->head = 0;
     lst->tail = 0;
     lst->free = 1;
@@ -135,23 +144,23 @@ void Dump_f (List_t* lst, const char* file,
              int line, const char* func_name, char* info,
              bool is_silent, bool no_corr, const char* file_name) {
     FILE* pFile = fopen (file_name, "ab");
-    /*
+
     const char* status = "ok";
     if (lst->is_valid != ok)                                                             //int or Elem_t????
         status = "NOT OK";
 
     if (!no_corr || lst->is_valid != 0) {
         fprintf (pFile, "%s from %s in line %d (file: %s)\n", info, func_name, line, file);
-        fprintf (pFile, "Stack_t <%s> [%x] (%s)\n", lst->name, lst, status);
+        fprintf (pFile, "List_t <%s> [%x] (%s)\n", lst->name, lst, status);
         fprintf (pFile, "{\n\tsize = %d\n\tdata[%d] = %x\n", lst->size, lst->max_size, lst->data);
         fprintf (pFile, "\t{\n");
 
         for (int i = 0; i < lst->size; ++i) {
-            fprintf (pFile, "\t* [%d] = %d\n", i, lst->data[i]);
+            fprintf (pFile, "\t* [%2d] = %3d;\t next = %2d;\t prev = %2d\n", i, lst->data[i], lst->next[i], lst->prev[i]);
         }
 
         for (int i = lst->size; i < lst->max_size; ++i) {
-            fprintf (pFile, "\t  [%d] = %d\n", i, lst->data[i]);
+            fprintf (pFile, "\t  [%2d] = %3d;\t next = %2d;\t prev = %2d\n", i, lst->data[i], lst->next[i], lst->prev[i]);
         }
         fprintf(pFile, "\t}\n");
 
@@ -161,36 +170,65 @@ void Dump_f (List_t* lst, const char* file,
             case nulldata:
                 fprintf (pFile, "Pointer to data is nullptr\n");
                 break;
-            case pop_last:
-                fprintf (pFile, "Trying to pop non-existing element\n");
-                break;
             case size_out_of_max:
                 fprintf (pFile, "Number of elements is bigger than max value\n");
                 break;
             case destructed:
-                fprintf (pFile, "Stack was destructed\n");
+                fprintf (pFile, "List was destructed\n");
                 break;
-                ON_DEBUG (case canary_t:
-                fprintf (pFile, "Top canary has been changed\n");
-                break;
-            case canary_b:
-                fprintf (pFile, "Bottom canary has been changed\n");
-                break;
-            case bad_hash:
-                fprintf (pFile, "Hash is not good\n");
-                break;
-                )
         }
-
-        ON_DEBUG (fprintf (pFile, "hash is now: %d\n", lst->hash);)
+        fprintf (pFile, "\thead = %d\n", lst->head);
+        fprintf (pFile, "\ttail = %d\n", lst->tail);
+        fprintf (pFile, "\tfree = %d\n", lst->free);
+        fprintf (pFile, "\tis_sorted = %d\n", lst->is_sorted);
         fprintf (pFile, "}\n\n\n");
     }
 
     if (!is_silent && lst->is_valid != 0) {
-        printf ("Abort was made. For info look in stack_dump.txt\n");
+        printf ("Abort was made. For info look in list_dump.txt\n");
         abort ();
     }
-     */
+
+    fclose (pFile);
+}
+
+void list_graph (List_t* lst, const char* pict_name, const char* pict_type) {
+    FILE* pFile = fopen ("list_graph.dot", "w");
+
+    fprintf (pFile, "digraph G{\nedge[color=\"chartreuse4\",fontcolor=\"blue\",fontsize=12];\nrankdir=LR;\nnode[shape=\"rectangle\",fontsize=15];\n");
+    fprintf (pFile, "%d[fillcolor=\"palegreen1\"];\n", lst->head);
+
+    int pos = lst->head;
+    while (lst->next[pos] != 0) {
+        fprintf (pFile, "%d->%d;\n", pos, lst->next[pos]);
+        pos = lst->next[pos];
+    }
+
+    fprintf (pFile, "edge[color=\"cyan3\",fontcolor=\"blue\",fontsize=12];\n");
+    pos = lst->tail;
+    while (lst->prev[pos] != 0) {
+        fprintf (pFile, "%d->%d;\n", pos, lst->prev[pos]);
+        pos = lst->prev[pos];
+    }
+
+    fprintf (pFile, "edge[color=\"palevioletred4\",fontcolor=\"blue\",fontsize=12];\n");
+    pos = lst->free;
+    while (lst->next[pos] != 0) {
+        fprintf (pFile, "%d->%d;\n", pos, lst->next[pos]);
+        pos = lst->next[pos];
+    }
+
+    fprintf (pFile, "}");
+    fclose (pFile);
+
+    //executing command in terminal
+    char command[60] = "dot list_graph.dot -T ";
+    strcat (command, pict_type);
+    strcat (command, " -o ");
+    strcat (command, pict_name);
+
+    system (command);
+
 }
 
 //‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
@@ -226,6 +264,57 @@ bool List_okey (List_t* lst) {
         return false;
     }
     return true;
+}
+
+void insert_after (List_t* lst, int pos, Elem_t elem) {
+    if (lst->prev[pos] == -1) {
+        printf ("Trying to insert element after non-existing element (it was not in list)\n");
+        lst->is_valid = calling_nowhere;
+    }
+    else {
+        int tmp = lst->next[lst->free];
+        //printf ("pos = %d = %d = tail\n", pos, lst->tail);
+
+        if (pos == lst->tail) {
+            lst->tail = lst->free;
+        }
+        else {
+            lst->is_sorted = false;
+        }
+
+        lst->prev[lst->next[pos]] = lst->free;
+        lst->data[lst->free] = elem;
+        lst->prev[lst->free] = pos;
+        lst->next[lst->free] = lst->next[pos];
+
+        lst->next[pos] = lst->free;
+
+        if (lst->head == 0) {
+            lst->head = lst->free;
+        }
+
+        lst->free = tmp;
+    }
+    List_OK (*lst)
+}
+
+void delete_after (List_t* lst, int pos) {
+    if (lst->prev[pos] == -1) {
+        printf ("Trying to delete element after non-existing element (it was not in list)\n");
+        lst->is_valid = calling_nowhere;
+    }
+    else {
+        int tmp = lst->next[pos];
+        lst->next[pos] = lst->next[lst->next[pos]];
+        lst->prev[lst->next[pos]] = pos;
+
+        lst->data[tmp] = 0;
+        lst->prev[tmp] = -1;
+
+        lst->next[tmp] = lst->free;
+        lst->free = tmp;
+    }
+    List_OK (*lst)
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -4,6 +4,20 @@
 int main() {
     List_t lst = {};
     Lt_construct (lst)
+    insert_after (&lst, 0, 11);
+    insert_after (&lst, 1, 22);
+    list_graph (&lst, "pict1.png");
+
+    insert_after (&lst, 1, 33);
+    list_graph (&lst, "pict2.png");
+    insert_after (&lst, 1, 44);
+    insert_after (&lst, 1, 55);
+
+    list_graph (&lst, "pict3.png");
+
+    delete_after (&lst, 1);
+
+    list_graph (&lst, "pict4.png");
 
     Lt_destruct (&lst);
 
